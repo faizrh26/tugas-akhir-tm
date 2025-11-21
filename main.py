1f56f5d060535c75a68b9053cabbdb4506713fc7
Haindz <hafidzindra54@gmail.com>
2025-11-21 15:50:18 +0700

    Fix: memperbaiki file main program (main.py)

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 import input_handler
-import preprocessing
+import prepocessing
+import extraction
+import scorer
 
 def main():
     # Step 1: INPUT HANDLING
     cv_text = input_handler.load_cv("data/cv_user.pdf")  # bisa diganti ke docx/txt
     interview_answers = input_handler.load_interview_answers("data/jawaban_user.csv")
-    rubric = input_handler.load_rubric("data/rubrik_penilaian.csv")
+    rubric = scorer.load_rubric("data/rubrik_penilaian.json")
 
     # Step 2: PREPROCESSING
-    processed_cv = preprocessing.clean_text(cv_text)
-    processed_answers = [preprocessing.clean_text(j) for j in interview_answers]
+    processed_cv = prepocessing.clean_text(cv_text)
+    processed_answers = [prepocessing.clean_text(j) for j in interview_answers]
 
+    # Step 3: EXTRACTION
+    entities = extraction.extract_entities(processed_cv)
+    keywords = extraction.extract_keywords(processed_cv)
+
+     # Step 4: SCORING
+    score_result = scorer.score_candidate(keywords, entities, rubric)
+    best_role = scorer.recommend_role(score_result)
+    
+     # Step 5: OUTPUT
+    print("===== BEST ROLE RECOMMENDATION =====")
+    print(best_role.upper())
+    print(score_result)
+    
     # Step 3: Output sementara (testing)
-    print("====Cleaned CV====")
-    print(processed_cv[:500])  # preview
-    print("====Cleaned Interview====")
-    for ans in processed_answers:
-        print(ans[:200])
+    # print("====Cleaned CV====")
+    # print(processed_cv[:500])  # preview
+    # print("====Cleaned Interview====")
+    # for ans in processed_answers:
+    #     print(ans[:200])
 
     # Lanjut ke extraction, scoring, visualisasi di file lain
 
