026f6a3d4366684e8fab985aa4a7b0cdd9b031b5
faizrh <faizradityahadi@gmail.com>
2025-11-22 15:16:49 +0700

    refactor main.py

--- main.py
+++ main.py
@@ -1,39 +1,17 @@
-import input_handler
-import prepocessing
-import extraction
-import scorer
+# main.py
+
+from pipeline import process_cv
 
 def main():
-    # Step 1: INPUT HANDLING
-    cv_text = input_handler.load_cv("data/user_cv.txt")  # bisa diganti ke docx/txt
-    # interview_answers = input_handler.load_interview_answers("data/jawaban_user.csv")
-    rubric = scorer.load_rubric("data/rubrik_penilaian.json")
+    file_path = "data/user_cv.txt"  #File path default
+    result = process_cv(file_path)
 
-    # Step 2: PREPROCESSING
-    processed_cv = prepocessing.clean_text(cv_text)
-    # processed_answers = [prepocessing.clean_text(j) for j in interview_answers]
-
-    # Step 3: EXTRACTION
-    entities = extraction.extract_entities(processed_cv)
-    keywords = extraction.extract_keywords(processed_cv)
-
-     # Step 4: SCORING
-    score_result = scorer.score_candidate(keywords, entities, rubric)
-    best_role = scorer.recommend_role(score_result)
-    
-     # Step 5: OUTPUT
-    print("===== BEST ROLE RECOMMENDATION =====")
-    print(best_role.upper())
-    print(score_result)
-    
-    # Step 3: Output sementara (testing)
-    # print("====Cleaned CV====")
-    # print(processed_cv[:500])  # preview
-    # print("====Cleaned Interview====")
-    # for ans in processed_answers:
-    #     print(ans[:200])
-
-    # Lanjut ke extraction, scoring, visualisasi di file lain
+    print("Job Role Recommendation")
+    print(result["best_role"].upper())
+    print()
+    print("Raw scores:", result["score_result"]["raw_scores"])
+    print("Percent   :", result["score_result"]["percent"])
+    print("Recommendation(s) are based on program reading on CV.")
 
 if __name__ == "__main__":
     main()
